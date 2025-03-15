cd50dba46aa60cba19a2ba8477e7fa2f90051610
Alina Oganina <ifoxspirit@yandex.ru>
2025-03-15 16:07:35 +0500

    fix

--- tests_test/tests/test_student_reg.py
+++ tests_test/tests/test_student_reg.py
@@ -24,5 +24,5 @@ def test_registers_user():
      .fill_state('NCR')
      .fill_city('Delhi')
      .click_submit_button()
-     .should_have_registered('my_firstName', 'my_secondName', 'my_email@mail.com', 'Male', '8800555353', '26 September 1999',
+     .should_have_registered('my_firstName', 'my_secondName', 'my_email@mail.com', 'Male', '8800555353', lambda text: '26 September' in text and '1999' in text,
                              'Maths', 'Sports', 'images.jpg', 'Abaya26', 'NCR', 'Delhi'))
